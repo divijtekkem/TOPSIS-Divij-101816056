1b6756c0ac8feedba213e7e3fd66c11e955e0e92
divijtekkem <69142321+divijtekkem@users.noreply.github.com>
2020-11-13 23:18:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'Topsis_ArjunMalik_101816029',         # How you named your package folder (MyLib)
-  packages = ['Topsis_ArjunMalik_101816029'],   # Chose the same as "name"
+  name = 'Topsis_Divij_101816056',         # How you named your package folder (MyLib)
+  packages = ['Topsis_Divij_101816056'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This library helps in implementing topsis method',   # Give a short description about your library
-  author = 'Arjun Malik',                   # Type in your name
-  author_email = 'amalik_be18@thapar.edu',      # Type in your E-Mail
+  author = 'Divij Tekkem',                   # Type in your name
+  author_email = 'dtekkem_be18@thapar.edu',      # Type in your E-Mail
   url = 'https://github.com/ARJUN-MALIK/Topsis-ArjunMalik-101816029',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Topsis-ArjunMalik-101816029/archive/1.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/divijtekkem/TOPSIS-Divij-101816056/archive/v1.0.tar.gz',    # I explain this later on
   keywords = ['SIMPLE', 'TOPSIS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'os',
